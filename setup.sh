4718a63475f95de341e2537b05336e1251385c92
neevek <i@neevek.net>
2023-05-03 14:42:52 +0800

    refactor configurations

--- setup.sh
+++ setup.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
 
+# git submodule add https://github.com/alacritty/alacritty-theme alacritty/alacritty-theme
+# git submodule add https://github.com/tmux-plugins/tpm tmux/plugins/tpm
+
+which tmux > /dev/null 2>&1
+if [[ "$?" != "0" ]]; then
+ echo "Install tmux first"
+ exit 1
+fi
+
+which zsh > /dev/null 2>&1 || sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+if [[ `uname -s` = "Linux" ]]; then
+  perl -pi -e 's/set-option -g prefix C-x/set-option -g prefix C-z/' ./tmux/tmux.conf
+fi
+
 current_dir=`pwd`
-mkdir ~/.config
+mkdir -p ~/.config
+
+ln -sfn $current_dir/zsh/neevek.zsh-theme ~/.oh-my-zsh/themes/neevek.zsh-theme
+ln -sfn $current_dir/git/gitconfig ~/.gitconfig
 ln -sfn $current_dir/alacritty ~/.config/alacritty
 ln -sfn $current_dir/tmux ~/.config/tmux
+ln -sfn $current_dir/lvim ~/.config/lvim
 # use "C-x I" to initialize tmux
 
-git submodule add https://github.com/alacritty/alacritty-theme alacritty/alacritty-theme
-git submodule add https://github.com/tmux-plugins/tpm tmux/plugins/tpm
+echo "Pulling submodules..."
+git submodule update --init
+git submodule update --remote --merge
+
+echo "Update zsh theme to 'neevek' in ~/.zshrc, and source it"
+echo "Done!"
