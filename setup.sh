5359fea289c59a2c4e2867295ceb4db44ed78855
Jiamin.Xie <jiamin.xjm@alibaba-inc.com>
2025-03-08 10:48:46 +0800

    update dotfiles

--- setup.sh
+++ setup.sh
@@ -22,14 +22,15 @@ ln -sfn $current_dir/zsh/neevek.zsh-theme ~/.oh-my-zsh/themes/neevek.zsh-theme
 ln -sfn $current_dir/git/gitconfig ~/.gitconfig
 ln -sfn $current_dir/alacritty ~/.config/alacritty
 ln -sfn $current_dir/tmux ~/.config/tmux
-ln -sfn $current_dir/lvim ~/.config/lvim
-# use "C-x I" to initialize tmux
+
+tmux run-shell ./tmux/plugins/tpm/bin/install_plugins && tmux source ./tmux/tmux.conf
+# or we can manually do the above using "C-x I" to initialize tmux
 
 echo "Pulling submodules..."
-git submodule update --init alacritty/alacritty-theme
-git submodule update --remote --merge alacritty/alacritty-theme
-git submodule update --init tmux/plugins/tpm
-git submodule update --remote --merge tmux/plugins/tpm
+git submodule update --init --recursive
+git submodule foreach --recursive git submodule update --remote --merge
+git submodule update --recursive --checkout
 
-echo "Update zsh theme to 'neevek' in ~/.zshrc, and source it"
-echo "Done!"
+sed -i -e 's/ZSH_THEME=".*"/ZSH_THEME="neevek"/' ~/.zshrc
+
+echo -e "\nDone! Remember to run: source ~/.zshrc"
